bd20b1b9310a7bc2cca7edcccef81d6488b056ff
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2023-12-01 15:50:52 +0100

    Fixed library docs

--- lib/resultify.dart
+++ lib/resultify.dart
@@ -1,8 +1,7 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// The `resultify` library provides a generic `Result<R, E>` type for result-oriented programming in Dart.
+/// It introduces convenient methods for handling success and error outcomes, allowing developers to streamline
+/// error management and enhance code clarity. The library is designed to improve the robustness and expressiveness
+/// of Dart applications by offering a concise and flexible approach to result handling.
 library;
 
 export 'src/resultify_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
